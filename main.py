b7870c222bf078a7ceed4ae0b990bd71283df56a
fbnrckshsr <68230409+fbnrckshsr@users.noreply.github.com>
2020-09-05 21:57:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import random
-import curses
+# imports
+import pygame
+import sys
 
-s = curses.initscr()
+particle = 25
+snek = [[13, 13], [13, 14]]
+direction = 0
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode([700, 700])
